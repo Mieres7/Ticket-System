f9d3b925c432f57f158522a3897294a44bb9859c
Mieres7 <vicente.mieres@usach.cl>
2024-01-08 22:46:45 -0300

    re-created backend proyect

--- ticketsystem/src/test/java/codesyncgroup/ticketsystem/TicketsystemApplicationTests.java
+++ ticketsystem/src/test/java/codesyncgroup/ticketsystem/TicketsystemApplicationTests.java
@@ -1,10 +1,10 @@
-package codesyncgroup.backend;
+package codesyncgroup.ticketsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class TicketsystemApplicationTests {
 
 	@Test
 	void contextLoads() {
