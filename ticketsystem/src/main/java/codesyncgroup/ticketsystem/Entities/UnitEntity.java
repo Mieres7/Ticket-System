c6fc353fc6055c9e770a64ce3fb8e1f56ef5deac
Mieres7 <vicente.mieres@usach.cl>
2024-01-09 23:12:57 -0300

    feat: created Unit entity

---
+++ ticketsystem/src/main/java/codesyncgroup/ticketsystem/Entities/UnitEntity.java
@@ -0,0 +1,10 @@
+package codesyncgroup.ticketsystem.Entities;
+
+public class UnitEntity {
+
+    // Atributes
+    private long idUnit;
+    private String unitName;
+    private String unitDescription;
+
+}
